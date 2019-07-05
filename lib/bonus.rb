497cf2c2b092be968f4906db5688b7b78e94896e
Bernadette Davis <bsd231@nyu.edu>
2019-07-05 14:04:46 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,7 @@ def bonus
   #code your solution here:
   #epic_tragedy[0[2[2]]] = "dead"
   #epic_tragedy[1[2[2]]] = "dead"
-  epic_tragedy[:montague[:hero[:status]]] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
   #epic_tragedy[:capulet[:heroine[:status]]] = "dead"
 
 
